1e6ccb2bb7314e055afa6e245da0466ce26121e7
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2024-01-19 19:27:37 -0800

    Added middleware

--- server.js
+++ server.js
@@ -3,3 +3,9 @@ const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001; 
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+

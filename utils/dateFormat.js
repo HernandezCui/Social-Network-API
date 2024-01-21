05de7c84238e04f322bc3e93864c998da1dd4571
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2024-01-21 13:57:13 -0800

    finished formating date

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -24,3 +24,22 @@ module.exports = (timestamp, { monthLength = 'short', dateSuffix = true } = {}) 
     const monthsLong = [
       'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
     ];
+
+    const dateObj = new Date(timestamp);
+    const months = monthLength === 'short' ? monthsShort : monthsLong;
+    const formattedMonth = months[dateObj.getMonth()];
+
+    const dayOfMonth = dateSuffix ? addDateSuffix(dateObj.getDate()) : dateObj.getDate();
+    const year = dateObj.getFullYear();
+
+    let hour = dateObj.getHours() > 12 ? Math.floor(dateObj.getHours() / 2) : dateObj.getHours();
+    hour = hour === 0 ? 12 : hour;
+
+    const minutes = dateObj.getMinutes();
+
+    const periodOfDay = dateObj.getHours() >= 12 ? 'pm' : 'am';
+
+    const formattedTimeStamp = `${formattedMonth} ${dayOfMonth}, ${year} at ${hour}:${minutes < 10 ? '0' : ''}${minutes} ${periodOfDay}`;
+
+    return formattedTimeStamp;
+};

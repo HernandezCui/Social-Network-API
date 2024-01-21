be19cf7255239c0641416055475385c2f832a213
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2024-01-21 15:50:35 -0800

    added outline for thoughts schema

---
+++ models/thought.js
@@ -0,0 +1,9 @@
+const { Schema, model } = require('mongoose');
+const reactionShema = require('./reaction');
+const dateFormat = require('../utils/date_format');
+
+const thoughtSchema = new schema(
+    {
+        text: 
+    }
+)

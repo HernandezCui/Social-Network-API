f519bedea1730d05b9a361b80a6c80399719b27a
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2024-01-21 16:35:14 -0800

    finished thought.js

--- models/thought.js
+++ models/thought.js
@@ -2,8 +2,37 @@ const { Schema, model } = require('mongoose');
 const reactionShema = require('./reaction');
 const dateFormat = require('../utils/date_format');
 
-const thoughtSchema = new schema(
+const thoughtSchema = new Schema(
     {
-        text: 
-    }
-)
+        text: {
+          type: String,
+          required: 'You need to leave a thought!',
+          minlength: 1,
+          maxlength: 280
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+          get: timestamp => dateFormat(timestamp)
+        },
+        author: {
+          type: String,
+          required: true
+        },
+        reactions: [reactionShema],
+      },
+      {
+        toJSON: {
+          getters: true
+        },
+        id: false
+      }
+    );
+    
+    thoughtSchema.virtual('reactionCount').get(function() {
+      return this.reactions.length;
+    });
+    
+    const Thought = model('Thought', thoughtSchema);
+    
+module.exports = Thought;

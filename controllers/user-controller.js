c7a7a85e8b1b217608c50df1e681547ec27ac5a2
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2024-01-21 20:59:04 -0800

    updated user by id

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -42,6 +42,24 @@ getUserById({ params }, res) {
       .catch((err) => res.status(400).json(err));
   },
 
+// PUT to update user by _id
+updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.userId }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  },
+
 
 
 

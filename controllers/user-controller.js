2aef0871dfa6f5eae5a4a8f06661f6854cdce263
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2024-01-21 20:11:03 -0800

    getting all users

---
+++ controllers/user-controller.js
@@ -0,0 +1,25 @@
+const { User } = require("../models");
+
+const userController = {
+  // GET all users
+  getAllUsers(req, res) {
+    User.find({})
+      .sort({ _id: -1 })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+
+
+
+
+
+
+
+  
+}
+
+module.exports = userController;

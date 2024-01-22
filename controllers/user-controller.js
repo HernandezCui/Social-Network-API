91b6f4d55c11753358b80a2195a62b09813a923d
(Cuitlahuac Hernandez) <hernandeztorrescui@gmail.com>
2024-01-21 21:22:43 -0800

    added delete function to remove friend from users friend list

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -92,10 +92,16 @@ addFriend({ params, body }, res) {
       .catch((err) => res.json(err));
   },
 
-
-
-
-
-}
+// DELETE to remove friend from user's friend list
+removeFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
+};
 
 module.exports = userController;
